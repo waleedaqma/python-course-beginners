c32f21107fee4c1eb7d85f217b83eb52a0d10e1c
Raza Aqma <aqmaraza@gmail.com>
2025-08-19 16:43:29 -0400

    I have completed my first project task and print 3 different lines like first one use how are you doing and second one about program is an instruction that tells comp what to do and the lst one is my age and favorite number

--- Raza.py
+++ Raza.py
@@ -1,1 +1,3 @@
-print("Hi All, How are you doing")
+print("Greeting waleed how are you doing!")
+print("A program is an instruction that tells comp what to do!")
+print("I'm 22 years old and favorite number is 18851!")
