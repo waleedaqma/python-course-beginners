ed0933ea0f21fcc992af906d18ab57305ea013d6
aunaqma-sys <aunaqma@gmail.com>
2025-08-21 17:02:05 -0400

    # Combine all information

--- aun.py
+++ aun.py
@@ -7,3 +7,5 @@ print("Your favorite hobby is " + user_hobby + ".")
 #add user-city
 user_city = input("Which city do you live in? ")
 print("You live in " + user_city + ".")
+# Combine all information
+print(user_name + " loves to " + user_hobby + " in " + user_city + ".")
