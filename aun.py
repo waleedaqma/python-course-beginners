eaaf8496de56ea841ab85ca1a0be1639086b79af
aunaqma-sys <aunaqma@gmail.com>
2025-08-21 16:52:07 -0400

    Add user-name

--- aun.py
+++ aun.py
@@ -1,7 +1,9 @@
-
+#add user-name
 user_name = input("What's your name? ")
 print("Hello, " + user_name + "!")
+#add hobby
 user_hobby = input("What is your favorite hobby? ")
 print("Your favorite hobby is " + user_hobby + ".")
+#add user-city
 user_city = input("Which city do you live in? ")
 print("You live in " + user_city + ".")
