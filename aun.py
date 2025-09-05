df896f3d2ac936d04b0d8d1a0ff2695aaf9ce0d2
aunaqma-sys <aunaqma@gmail.com>
2025-09-05 17:22:53 -0400

    Create a basic Python script to calculate student percentage and assign grade

--- aun.py
+++ aun.py
@@ -1,10 +1,16 @@
+# A simple program to calculate percentage and grade based on marks obtained
 name = input("Enter your name: ")
+# Student Information
 age = int(input("Enter your age: "))
+# Marks Information
 total_marks = 1200
+# Get marks obtained from user
 marks_obtained = int(input("Enter the marks obtained: "))
+# Calculate percentage
 percentage = (marks_obtained / total_marks) * 100
+# Display percentage
 print("Percentage:", percentage) 
-
+# Display grade
 if percentage >= 90:
     print("Grade: A")
 elif percentage >= 80:
