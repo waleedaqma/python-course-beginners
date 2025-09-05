b5d6de583ed46da1fe5514485ec4f540c638870e
aunaqma-sys <aunaqma@gmail.com>
2025-09-05 16:49:06 -0400

    Prompt user to enter their name via input

--- aun.py
+++ aun.py
@@ -1,11 +1,15 @@
-#add user-name
-user_name = input("What's your name? ")
-print("Hello, " + user_name + "!")
-#add hobby
-user_hobby = input("What is your favorite hobby? ")
-print("Your favorite hobby is " + user_hobby + ".")
-#add user-city
-user_city = input("Which city do you live in? ")
-print("You live in " + user_city + ".")
-# Combine all information
-print(user_name + " loves to " + user_hobby + " in " + user_city + ".")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+total_marks = 1200
+marks_obtained = int(input("Enter the marks obtained: "))
+percentage = (marks_obtained / total_marks) * 100
+print("Percentage:", percentage) 
+
+if percentage >= 90:
+    print("Grade: A")
+elif percentage >= 80:
+    print("Grade: B")
+elif percentage >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
