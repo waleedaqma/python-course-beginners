3f9204af4512ea5cd7eb35a319d678476037f77e
Asim Aqma <asimaqma@gmail.com>
2025-09-05 16:59:32 -0400

    obtain marks veriable created

--- AimL3test.py
+++ AimL3test.py
@@ -1,4 +1,6 @@
 student_name = input("enter student name" )
 student_age = int(input("enter your age" ))
-Total_marks = float(input("enter your total marks "))
-obtained_marks = float(input("enter"))
+Total_marks = float(input("enter total marks "))
+obtained_marks = float(input("enter your obtained marks"))
+
+persntage
