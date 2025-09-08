9eb42a324b93452d263ae1d47aae54cb81e4b6ea
qasimAqma <qasimaqma@gmail.com>
2025-09-08 15:46:44 -0400

    new program perfom

--- Ltest.py
+++ Ltest.py
@@ -1,3 +1,20 @@
-name =input("what's your name? ")
-age =input("how old are you? ")
-total marks =input("total marks? ")
+name = input("what's your name? ")
+age = input("how old are you? ")
+total_marks = int(input("total_marks? "))
+obtain_marks = int(input("obtain_marks "))
+
+percentage = (obtain_marks / total_marks) *100 
+
+print("n/=== results ===")
+print("your name is:", name)
+print("your age is:", age )
+print("total_marks:", total_marks)
+print("obtain_marks:", obtain_marks)
+print("percentage:", percentage, "%")
+
+if   percentage <= 20:
+     print("fail")
+elif percentage <=50:
+     print("grade") 
+else:
+     print("goood")  
