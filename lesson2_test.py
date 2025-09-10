b9a50e99a36c30083dca2f03893cf18e3d2089fc
ubaidaqma-hub <ubaidaqa@gmail.com>
2025-09-10 13:36:22 -0400

    Task completed, and I have used 2 commands: isalpha and assert. These define that inside the string line there should only be alphabets — no numbers, spaces, or any other special characters. isalpha defines that there must only be alphabets; if there aren’t, it will return false. And assert is a test that checks — if it’s correct, then the program will run normally, and if it’s not correct, it will give an error.

--- lesson2_test.py
+++ lesson2_test.py
@@ -1,30 +1,22 @@
-print("=== School Result ===")
+name = input("Enter your name: ")
+assert name.isalpha()
 
-name = input("What's your name: ")
-age = int(input("What's your age: "))
-total_marks = int(input("Total Marks? "))
-obtain_marks = int(input("Obtain Marks: "))
+age = int(input("Enter your age: "))
+total_marks = 100
+marks_obtained = int(input("Enter the marks obtained: "))
+percentage = (marks_obtained / total_marks) * 100
+print("Percentage:", percentage, "%") 
 
-percentage = (obtain_marks / total_marks) * 100
+print("Name: ", name)
+print("Age: ", age)
 
-print("\n=== Results ===")
-print("Your name is:", name)
-print("Your age is:", age)
-print("Total Marks:", total_marks)
-print("Obtained Marks:", obtain_marks)
-print("Percentage:", percentage, "%")
-
-if percentage < 20:
+if percentage <= 30:
     print("Fail")
-elif percentage < 30:
-    print("Fail")
-elif percentage < 50:
+elif percentage <= 50:
+    print("pass")
+elif percentage <= 60:
     print("Good")
-elif percentage < 60:
-    print("Good")
-elif percentage < 70:
-    print("Excellent")
-elif percentage < 80:
+elif percentage <= 80:
     print("Excellent")
 else:
     print("Sounds Good")
