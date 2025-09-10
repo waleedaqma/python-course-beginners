ae88aa62a7b60ceabe8b3543d39fa0e17caedd92
aunaqma-sys <aunaqma@gmail.com>
2025-09-10 15:38:21 -0400

    a simple program to calculate parcentage and based on masks obtained\

--- aun.py
+++ aun.py
@@ -1,5 +1,6 @@
 # A simple program to calculate percentage and grade based on marks obtained
-name = input("Enter your name: ")
+name = (input("Enter your name: "))
+print(name.isalpha())
 # Student Information
 age = int(input("Enter your age: "))
 # Marks Information
@@ -11,11 +12,11 @@ percentage = (marks_obtained / total_marks) * 100
 # Display percentage
 print("Percentage:", percentage) 
 # Display grade
-if percentage >= 90:
-    print("Grade: A")
-elif percentage >= 80:
-    print("Grade: B")
-elif percentage >= 70:
-    print("Grade: C")
+if percentage <= 50:
+    print("Grade: d")
+elif percentage <= 70:
+    print("Grade: c")
+elif percentage <= 80:
+    print("Grade: b")
 else:
-    print("Grade: D")
+    print("Grade: a")
