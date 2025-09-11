fc20c0452dc6fee7849ff56f30d0cdc1e8e8a969
qasimAqma <qasimaqma@gmail.com>
2025-09-11 09:10:59 -0400

    if not name.isalpha(): new line add

--- Ltest.py
+++ Ltest.py
@@ -1,8 +1,10 @@
-name = input("what's your name? ")
-  name.isalpha()
 
- 
-age = input("how old are you? ")
+name = input("what's your name? ")
+age = int(input("your age is? "))
+if not name.isalpha():
+    print("Invalid name! Only alphabets (A–Z) allowed.")
+    name = input("what's your name? ")
+
 total_marks = int(input("total_marks? "))
 obtain_marks = int(input("obtain_marks? "))
 
@@ -10,7 +12,7 @@ percentage = (obtain_marks / total_marks) *100
 
 print("n/=== results ===")
 print("your name is:", name)
-print("your age is:", age )
+print("your age is:", age)
 print("total_marks:", total_marks)
 print("obtain_marks:", obtain_marks)
 print("percentage:", percentage, "%")
