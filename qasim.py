4da003955756ddedbcc04a04f8979332ee70b697
qasimAqma <qasimaqma@gmail.com>
2025-09-11 16:05:16 -0400

    new program make thi

--- qasim.py
+++ qasim.py
@@ -1,3 +1,15 @@
-print("my name is qasim!"how are you?)
-print("i love python!")
-print("my age 32!"my favorite number is 9!")
+class Student:
+    def __init__(self, name, marks): 
+        self.name = name
+        self.marks = marks
+
+    def get_avg(self):
+        total = 0
+        for val in self.marks:
+            total += val
+        print("Hi", self.name, "your avg score is:", total/len(self.marks))  
+
+si = Student("Qasim Shah", [99, 98, 97])  
+si.get_avg()
+si.name = "Raza"
+si.get_avg()
