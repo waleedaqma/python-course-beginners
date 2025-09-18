2bc50ca23091c3f2846407321e156d32ebf23cf3
qasimAqma <qasimaqma@gmail.com>
2025-09-18 17:02:08 -0400

    if age 18

--- QT2.py
+++ QT2.py
@@ -5,4 +5,6 @@ name = input("what is your name? ")
 age = int(input("how old are you? ")
 favoritse_number = int(input("what is your favorite number? ")
 print(f"hello {name}, you are {age} years old and your favorite number is {favoritse_number}")
-if age >= 18
+if age >= 18:
+    print("you're an teenager")
+if favoritse_number == 7:
