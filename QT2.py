1f11541455f4dbe8c6a08a47f4d44f5f2ace11c0
qasimAqma <qasimaqma@gmail.com>
2025-09-18 16:40:13 -0400

    age how old are age add lin

--- QT2.py
+++ QT2.py
@@ -2,3 +2,7 @@ course_name = "python basics"
 lesson_number = 2
 is_fun = True
 name = input("what is your name? ")
+age = int(input("how old are you? ")
+favoritse_number = int(input("what is your favorite number? ")
+print(f"hello {name}, you are {age} years old and your favorite number is {favoritse_number}")
+if age >= 18
