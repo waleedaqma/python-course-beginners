f78f4c1ad5c55600fba7194bd10c5c91d545808f
aunaqma-sys <aunaqma@gmail.com>
2025-09-18 17:02:02 -0400

    to make a program name age frv number

---
+++ Test/AT2.py
@@ -0,0 +1,19 @@
+
+course_name = "Python Basics"
+lesson_number = 2
+is_fun = True
+print(f"{course_name} - Lesson{lesson_number} - Fun:{is_fun}")
+name = input("What is your name? ")
+age = input("How old are you? ")
+fav_number = input("What is your favorite number? ")
+age = int(age)
+fav_number = int(fav_number)
+print(f"\nHello{name}! You are{age}years old and your favorite number is{fav_number}.")
+print(f"Next year you will be{age + 3}.")
+print(f"Double your favorite number is{fav_number * 5}.")
+
+
+
+
+
+
