ca438879b6977443bec99cb44d709b417d2725d5
ubaidaqma-hub <ubaidaqa@gmail.com>
2025-09-18 16:39:25 -0400

    code result with one line space command line created

--- Test/UT2.py
+++ Test/UT2.py
@@ -5,3 +5,5 @@ is_fun = True
 name = input("whats your name? ")
 age = int(input("whats your age? "))
 favorite_number = int(input("whats your favorite number? "))
+
+print("\n==== code result ====")
