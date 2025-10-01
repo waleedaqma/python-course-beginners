2fb02ecda3798f70ff60100c9674a9e7ab0d3866
qasimAqma <qasimaqma@gmail.com>
2025-10-01 09:03:49 -0400

    RAZA SHAH  bank  account

--- QT2.py
+++ QT2.py
@@ -1,24 +1,26 @@
 class Account:
-    def __init__(self, balance, acc):
-        self.balance = balance
-        self.account_number = acc
+    def __init__(self, name, balance, acc):
+        self.name = name                 
+        self.balance = balance           
+        self.account_number = acc        
 
     def deposit(self, amount):
         self.balance += amount
-        print("Rs", amount, "was deposited")
-        print("total balance =", self.get_balance())
+        print(self.name, "deposited Rs", amount)
+        print("Total balance =", self.get_balance())
 
     def credit(self, amount):
         self.balance -= amount
-        print("Rs", amount, "was credited")
-        print("total balance =", self.get_balance()) 
+        print(self.name, "credited Rs", amount)
+        print("Total balance =", self.get_balance()) 
 
     def get_balance(self):
         return self.balance
 
 
-acc1 = Account(1000, "12345")
-acc1.deposit(500)   
-acc1.credit(200)    
-acc1.get_balance()        
-print("total balance =", acc1.get_balance())
+acc1 = Account("Razaq Shah", 1000000 , "12345")
+acc1.deposit(44499)   
+acc1.credit(500000)
+print("Final balance of", acc1.name, "=", acc1.get_balance())
+
+
