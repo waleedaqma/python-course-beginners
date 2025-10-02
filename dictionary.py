4747fe76b5b8ee8775568c45353bca5fb4d040ce
qasimAqma <qasimaqma@gmail.com>
2025-10-02 13:55:40 -0400

    dictionary create

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,12 @@
-f = open("computer.txt", "r")
-data = f. read()
-print(data)
-print(type(data))
-f.close()
- 
+info = {
+    "name" : "Qasim",
+  "subjects" : ["Python", "C", "java"],
+    "topics" : ("dict", "set"),
+    "age": 31,
+    "is adult": True,
+    12.99 : 94.4
+} 
+
+print(info["name"])
+print(info["subjects"])
+print(info["age"])
