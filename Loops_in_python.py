8c4fa44c074882265ca97426d564f8a182e3cafc
qasimAqma <qasimaqma@gmail.com>
2025-10-03 10:25:52 -0400

    while loops add

--- Loops_in_python.py
+++ Loops_in_python.py
@@ -1,8 +1,6 @@
-nums = [1, 2, 3 ,4 ,56, 76, 85, 100,]
 
-idx = 0
-while idx < len(nums):
-    print(nums[idx])
-    idx += 1
-                                
-                                
+heroes = ["Qasim", "Ahsan", "Ali", "Hassan", "Hussain", "raza", "Hadi", "Hussain"]
+i = 0
+while i < len(heroes):
+    print(heroes[i])
+    i += 1
